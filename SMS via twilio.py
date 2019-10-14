1d9af9212373f1b06146ea356a2f7759e24163b2
Swapnil Narayan <39152397+fswap@users.noreply.github.com>
2019-10-15 00:37:41 +0530

    Updated twilio file

--- SMS via twilio.py
+++ SMS via twilio.py
@@ -1,8 +1,6 @@
 from twilio.rest import Client
-
-
-# Your Account Sid and Auth Token from twilio.com/console
-# DANGER! This is insecure. See http://twil.io/secure
+# This belongs to Engigma Rebooted for the HackFest 2019
+# Project is Smart Blind Man Stick
 account_sid = 'AC3c6c12a72dcee8923300920080247869'
 auth_token = '1bf2bd405db1cbdb0713a4b73ed64f2a'
 client = Client(account_sid, auth_token)
